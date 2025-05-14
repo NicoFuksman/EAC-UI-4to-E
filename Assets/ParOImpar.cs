b2e639aabd429ceb610ce4d031c01b96dddfa614
NicoFuksman <bf0005@est.ort.edu.ar>
2025-05-14 16:00:06 -0300

    terminado

---
+++ Assets/ParOImpar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ParOImpar : MonoBehaviour
+{
+    public TMP_InputField inputNum1;
+    public TextMeshProUGUI txtParOIMpar;
+    int num1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtParOIMpar.text = string.Empty;
+
+    }
+    public void ESparONo()
+    {
+        if (inputNum1.text == "")
+        {
+            return;
+        }
+
+        num1 = int.Parse(inputNum1.text);
+
+        if (num1 <= 0)
+        {
+            txtParOIMpar.text = "El numero debe ser mayor que cero";
+            return;
+        }
+        if (num1 % 2 == 0)
+        {
+            txtParOIMpar.text = "El numero es par";
+
+        }
+
+        else
+        {
+            txtParOIMpar.text = "El numero es impar";
+        }
+    }
+}
